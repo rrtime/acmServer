322c3af83716331559299f291fb879443365263e
LiLei <1351284857@qq.com>
2016-06-15 14:16:36 +0800

    提交修改

--- acmServer/src/com/rrtimes/acm/action/SysUserAction.java
+++ acmServer/src/com/rrtimes/acm/action/SysUserAction.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import net.sf.json.JSONObject;
 
@@ -28,6 +29,7 @@ import com.rrtimes.acm.domain.AcmSysOrg;
 import com.rrtimes.acm.domain.AtCompanyAgent;
 import com.rrtimes.acm.domain.AtUser;
 import com.rrtimes.acm.domain.PageObject;
+import com.rrtimes.acm.serviceI.AcmSysOrgService;
 import com.rrtimes.acm.serviceI.AtUserGroupRelService;
 import com.rrtimes.acm.serviceI.AtUserService;
 
@@ -52,8 +54,12 @@ public class SysUserAction extends ActionSupport{
 	private static final long serialVersionUID = 2954363164266848728L;
 	@Resource
 	private AtUserService aus;
+	
 	@Resource 
 	private AtUserGroupRelService augrs;
+	
+	@Resource
+	private AcmSysOrgService asoservice;
 
 	private AtUser atUser = new AtUser();
 	
@@ -65,11 +71,19 @@ public class SysUserAction extends ActionSupport{
 	
 	private List<AtUser> list = new ArrayList<AtUser>();
 	
+	private AcmSysOrg org = new AcmSysOrg();
+	
 	private int rst;
 	
 	private int cmd;
 	
 	private String msg;
+	
+    private String oldpwd;
+	
+	private String newpwd;
+	
+	HttpSession session = ServletActionContext.getRequest().getSession();
 	
 	//登录
 	public String login(){
@@ -197,6 +211,66 @@ public class SysUserAction extends ActionSupport{
 		return "queryjsBycpCode";
 	}
 	
+	/**
+	 * 首页我的桌面模块
+	 * */
+	public String userSet()
+	{
+		atUser = (AtUser)session.getAttribute("loginUser");
+		org = asoservice.queryOrgById(atUser.getOid());
+		return "userset";
+	}
+	
+	//修改登录用户的基本信息
+	public String updateuser(){
+		AtUser au = (AtUser)session.getAttribute("loginUser");
+		au.setIname(atUser.getIname());
+		au.setSex(atUser.getSex());
+		au.setEmail(atUser.getEmail());
+		au.setTelno(atUser.getTelno());
+		au.setOid(atUser.getOid());
+		au.setRemark(atUser.getRemark());
+		boolean result = aus.queryByInameToupdate(au.getIname(), au.getId());
+		if(result){//如果当前用户名不重复
+			int i = aus.updateUser(au);
+			if(i==0){
+				msg="修改成功";
+			}else{
+				msg="修改失败";
+			}
+		}
+		return userSet();
+	}
+	
+	//修改密码
+	public void updatepassword(){
+		try{
+			HttpServletResponse response = ServletActionContext.getResponse();
+			PrintWriter out;
+			JSONObject jo = new JSONObject();
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			AtUser au = (AtUser)session.getAttribute("loginUser");
+			if(oldpwd.equals(au.getLoginPwd()) && newpwd.equals(atUser.getLoginPwd())){//如果输入的旧密码和原始密码相同且新密码和确认密码相同
+				au.setLoginPwd(atUser.getLoginPwd());
+				int i = aus.updateUser(au);
+				if(i==0){
+					msg="修改成功";
+				}else{
+					msg="修改失败";
+				}
+			}else{
+				msg="修改失败";
+			}
+			jo.put("status", msg );
+			out = response.getWriter();
+			out.print(jo);
+			out.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public AtUser getAtUser() {
 		return atUser;
 	}
@@ -260,5 +334,28 @@ public class SysUserAction extends ActionSupport{
 		this.page = page;
 	}
 
+	public AcmSysOrg getOrg() {
+		return org;
+	}
+
+	public void setOrg(AcmSysOrg org) {
+		this.org = org;
+	}
+
+	public String getOldpwd() {
+		return oldpwd;
+	}
+
+	public void setOldpwd(String oldpwd) {
+		this.oldpwd = oldpwd;
+	}
+
+	public String getNewpwd() {
+		return newpwd;
+	}
+
+	public void setNewpwd(String newpwd) {
+		this.newpwd = newpwd;
+	}
 	
 }
